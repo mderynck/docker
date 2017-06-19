9fd912164657463725ba3ed2be5956c175b8c199
mderynck <michael.derynck@gmail.com>
2017-06-18 23:20:57 -0400

    Fix nzbhydra run.sh

--- nzbhydra/run.sh
+++ nzbhydra/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-docker run -d --name pyload --restart unless-stopped \
-    -p 8000:8000 \
+docker run -d --name nzbhydra --restart unless-stopped \
+    -p 5075:5075 \
+    -v /mnt/docker/nzbhydra/config:/config \
+    -v /var/local/nzbhydra:/db \
+    -v /var/log:/log \
     -v /tmp:/tmp \
-    pyload
+    nzbhydra
